1ccecffd049d3d5446fa4b3078f8dd9dc21abb1b
Robert O'Shea <robertosheagamedev@gmail.com>
2024-09-29 21:55:18 +0100

    Update project to use Raylib cpp

--- src/game.cpp
+++ src/game.cpp
@@ -1,37 +1,34 @@
-#include "raylib.h"
+#include "raylib-cpp.hpp"
 
 #if defined(PLATFORM_WEB)
-    #include <emscripten/emscripten.h>
+#include <emscripten/emscripten.h>
 #endif
 
-static const int screenWidth = 800;
-static const int screenHeight = 450;
+int screenWidth = 800;
+int screenHeight = 450;
 
-static void UpdateDrawFrame(void);
+void UpdateDrawFrame(void);     // Update and Draw one frame
 
-int main(void)
+int main()
 {
-    InitWindow(screenWidth, screenHeight, "BoomHeadshot");
-
-    InitAudioDevice();
+    raylib::Window window(screenWidth, screenHeight, "Boom Headsht");
 
 #if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
 #else
     SetTargetFPS(60);
 
-    while (!WindowShouldClose())
+    while (!window.ShouldClose())
     {
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Hello World", screenWidth / 2, screenHeight / 2, 32, BLACK);
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Hello World!", screenWidth / 2 - 16, screenHeight / 2 - 16, 32, BLACK);
+
         EndDrawing();
     }
 #endif
-
-    CloseAudioDevice();
-
-    CloseWindow();
 
     return 0;
 }
