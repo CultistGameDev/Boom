afc5afe9016417ccf4862cdae7ec37c12e826ef0
Robert O'Shea <robertosheagamedev@gmail.com>
2024-10-01 20:15:46 +0100

    Update color.hpp

--- include/color.hpp
+++ include/color.hpp
@@ -4,8 +4,14 @@
 #include "vector.hpp"
 
 namespace BH {
-using Colorf = Vec4f;
-using Colori = Vec4i;
+using Color2f = Vec2f;
+using Color3f = Vec3f;
+using Color4f = Vec4f;
+
+using Color2i = Vec2i;
+using Color3i = Vec3i;
+using Color4i = Vec4i;
+
 } // namespace BH
 
 #endif // BOOMHEADSHOT_COLOR_HPP
