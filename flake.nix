52dab691d250d95d41390b1ef5d6221f1f29685c
RobertGameDev <robertosheagamedev@gmail.com>
2024-10-02 21:27:30 +0100

    Added nix support

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+  	nativeBuildInputs = with pkgs; [
+	    pkg-config
+	    cmake
+	    gnumake
+	    clang
+	    clang-tools
+
+	    xorg.libX11
+	    xorg.libXrandr
+	    xorg.libXinerama
+	    xorg.libXcursor
+	    xorg.libXi
+	];
+        default = pkgs.mkShell.override
+	  {
+
+	  }
+	  {
+	  packages = with pkgs; [
+	     glfw
+	  ] ++ (if system == "aarch64-darwiu" then [] else [gdb]);
+	  };
+      });
+    };
+}
