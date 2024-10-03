00c753d40270570ad535788dcfb9bcd7dc9343fa
RobertGameDev <robertosheagamedev@gmail.com>
2024-10-03 20:23:13 +0100

    Temporary commit

--- src/game.cpp
+++ src/game.cpp
@@ -1,37 +1,23 @@
-#include <vector>
-
 #if defined(PLATFORM_WEB)
 #include <emscripten/emscripten.h>
 #endif
 
-#include "color.hpp"
-#include "light.hpp"
-#include "vector.hpp"
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
 constexpr int screenWidth = 800;
+
 constexpr int screenHeight = 450;
 
 int main(int argc, char **argv) {
 #if defined(PLATFORM_WEB)
     emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
 #else
-
-    BH::Vec2f v(0.1f, 1.2f);
-    v.X(2.4f);
-
-    BH::Color4f ambient{0.1f, 0.1f, 0.1f, 1.0f};
-
-    std::vector<BH::Light> lights;
-    lights.reserve(4);
-    lights.emplace_back(BH::LightType::LIGHT_POINT, BH::Vec3f{-2, 1, -2},
-                        BH::Vec3f(), BH::Color4i{253, 249, 0, 255});
-    lights.emplace_back(BH::LightType::LIGHT_POINT, BH::Vec3f{2, 1, 2},
-                        BH::Vec3f(), BH::Color4i{253, 249, 0, 255});
-    lights.emplace_back(BH::LightType::LIGHT_POINT, BH::Vec3f{-2, 1, 2},
-                        BH::Vec3f(), BH::Color4i{230, 41, 55, 255});
-    lights.emplace_back(BH::LightType::LIGHT_POINT, BH::Vec3f{2, 1, -2},
-                        BH::Vec3f(), BH::Color4i{41, 230, 55, 255});
-
+    glfwInit();
 #endif
     return 0;
 }
