09387a8320c12b3739348c95007001ad9511c68d
RobertGameDev <robertosheagamedev@gmail.com>
2024-10-04 03:02:56 +0100

    Create a Vulkan Instance

--- src/game.cpp
+++ src/game.cpp
@@ -1,3 +1,4 @@
+#include <vulkan/vulkan_core.h>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
@@ -6,32 +7,129 @@
 #include <glm/mat4x4.hpp>
 #include <glm/vec4.hpp>
 
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
+#include <stdexcept>
+#include <vector>
 
-constexpr int screenWidth = 800;
-constexpr int screenHeight = 450;
+class HelloTriangleApplication {
+public:
+  void run() {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
 
-int main(int argc, char **argv) {
+private:
+  const uint32_t WIDTH = 800;
+  const uint32_t HEIGHT = 600;
+
+  GLFWwindow *window;
+
+  VkInstance instance;
+
+  void initWindow() {
     glfwInit();
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow *window =
-        glfwCreateWindow(800, 600, "Vulkan Window", nullptr, nullptr);
 
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
+  }
+
+  void initVulkan() { createInstance(); }
+
+  void createInstance() {
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pEngineName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char **glfwExtensions;
+    std::vector<const char *> requiredExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    requiredExtensions.reserve(glfwExtensionCount);
+    for (uint32_t i = 0; i < glfwExtensionCount; ++i) {
+      requiredExtensions.emplace_back(glfwExtensions[i]);
+    }
+
+#ifdef __APPLE__
+    requiredExtensions.emplace_back(
+        VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+    createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+#endif
+
+    createInfo.enabledExtensionCount = (uint32_t)requiredExtensions.size();
+    createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+    createInfo.enabledLayerCount = 0;
+
+    if (!validateExtensions(requiredExtensions)) {
+      throw std::runtime_error("Can't find all extensions");
+    }
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+      throw std::runtime_error("Failed to create instance!");
+    }
+  }
+
+  bool validateExtensions(const std::vector<const char *>& extensionNames) {
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    std::vector<VkExtensionProperties> extensions(extensionCount);
 
-    std::cout << extensionCount << " extensions supported\n";
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount,
+                                           extensions.data());
 
-    glm::vec4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    bool found = false;
+    for (const auto& reqExtension : extensionNames) {
+      found = false;
+      for (const auto& extension : extensions) {
+        if (std::strcmp(extension.extensionName, reqExtension) == 0) {
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        break;
+      }
     }
+    return found;
+  }
+
+  void mainLoop() {
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+    }
+  }
+
+  void cleanup() {
+    vkDestroyInstance(instance, nullptr);
 
     glfwDestroyWindow(window);
     glfwTerminate();
+  }
+};
 
-    return 0;
+int main(int argc, char **argv) {
+  HelloTriangleApplication app;
+
+  try {
+    app.run();
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
 }
